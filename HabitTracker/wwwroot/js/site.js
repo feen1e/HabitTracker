3471110e6cf788cbba45dbb41fc171a8b2e6a7ca
Dominik Kaczmarek <d.kaczmarek04@gmail.com>
2024-06-23 16:00:35 +0200

    edited details

--- HabitTracker/wwwroot/js/site.js
+++ HabitTracker/wwwroot/js/site.js
@@ -7,3 +7,24 @@ setInterval(function () {
     var b = Math.floor(Math.random()*100);
     document.body.style.backgroundPosition = a+"% " + b+"%";
 }, 5000)
+
+
+const buttons = document.querySelectorAll('.completion-status');
+buttons.forEach(button => {
+    var defaultText = button.value;
+    
+    button.addEventListener('mouseover', function() {
+        if (button.value === "Habit inactive") {
+            return;
+        }
+        if (button.value === "Completed") {
+            button.value = "Mark as incomplete"
+        } else {
+            button.value = "Mark as completed"
+        }
+    });
+
+    button.addEventListener('mouseout', function() {
+        button.value = defaultText;
+    });
+});
